66860380e46e18ecb7e115cb8dafd7b8eb04a03d
Zhecho <zhechev.zhecho@gmail.com>
2019-07-08 12:33:57 +0300

    - change "skeleton" in setup 	modified:   requirements.txt 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -5,27 +5,28 @@ import uuid
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-__author__ = 'David Barroso <dbarrosop@dravetech.com>'
+__author__ = 'Zhecho Zhechev <zhechev.zhecho@gmail.com>'
 
 install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
-    name="napalm-skeleton",
+    name="napalm-fortinet",
     version="0.1.0",
     packages=find_packages(),
-    author="David Barroso",
-    author_email="dbarrosop@dravetech.com",
+    author="Zhecho Zhechev",
+    author_email="zhechev.zhecho@gmail.com",
     description="Network Automation and Programmability Abstraction Layer with Multivendor support",
     classifiers=[
         'Topic :: Utilities',
          'Programming Language :: Python',
          'Programming Language :: Python :: 2',
          'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS',
     ],
-    url="https://github.com/napalm-automation/napalm-skeleton",
+    url="https://github.com/zhecho/napalm-fortinet",
     include_package_data=True,
     install_requires=reqs,
 )
